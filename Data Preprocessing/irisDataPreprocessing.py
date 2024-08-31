5fb97ff931dc823646d616da977f8987ac4fd690
plamenstoynev <plamen.stoynev24@gmail.com>
2024-08-31 13:52:06 +0300

    initial
    initial

---
+++ Data Preprocessing/irisDataPreprocessing.py
@@ -0,0 +1,15 @@
+# Importing the necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Loading the Iris dataset
+dataset = pd.read_csv('/Users/plamenstoynev/OneDrive/Python/Data Preprocessing/iris.csv')
+
+# Creating the matrix of features (X) and the dependent variable vector (y)
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Printing the matrix of features and the dependent variable vector
+print(y)
+print(x)
